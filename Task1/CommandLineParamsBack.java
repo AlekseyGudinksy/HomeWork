05f30f5a9822f61df258ac85dfdafd461e1fbd14
Aleksey <alex@MacBook-Pro-Aleksey.local>
2016-10-03 02:07:35 +0300

    JavaDoc was fixed

---
+++ Task1/CommandLineParamsBack.java
@@ -0,0 +1,19 @@
+/**
+ * Output parameters
+ * of command line
+ * in the opposite order
+ **/
+public class CommandLineParamsBack {
+    /**
+     * Calling method main
+     * which run along
+     * the whole length
+     * of the array "args"
+     * including last element "0"
+     **/
+    public static void main(String[] args) {
+        for (int i = args.length - 1; i >= 0; i--) {
+            System.out.println("Parameter " + i + " = " + args[i]);
+        }
+    }
+}
