5d614481037dda574d1a1ed111871f81afee6cde
Aleksey <alex@MacBook-Pro-Aleksey.local>
2016-10-21 02:12:11 +0300

    added @author in JavaDock

--- Task1/CommandLineParamsBack.java
+++ Task1/CommandLineParamsBack.java
@@ -1,13 +1,16 @@
 /**
- * Output parameters of command line in the opposite order
+ * Main is an entry point.
+ *
+ * @author Aleksey Gudinsky
  */
 public class CommandLineParamsBack {
     /**
-     * Calling method main which run along the whole length of the array "args"
-     * including last element "0"
+     * Running along the whole length of the array "args"
+     * Output arguments of command line in opposite order
      *
      * @param args is input arguments
      */
+
     public static void main(String[] args) {
         for (int i = args.length - 1; i >= 0; i--) {
             System.out.println("Parameter " + i + " = " + args[i]);
