3c920497cdf1c9aa591b7a8b57ea5506a47ddba9
Aleksey <alex@MacBook-Pro-Aleksey.local>
2016-10-21 02:01:52 +0300

    Javadock changed. Added branch else

--- Task2/HelloName.java
+++ Task2/HelloName.java
@@ -1,17 +1,21 @@
 /**
- * Class which output Hello, Name, Name takes as arguments from command line.
+ * Method main is point of entry.
+ *
+ * @author Aleksey Gudinsky
  */
 public class HelloName {
     /**
-     * Method main is point of entry.
+     * Output Hello, Name. Name takes as argument from command line.
      *
      * @param args is input argument from command line
      */
     public static void main(String[] args) {
-        System.out.print("Hello, ");
-        for (int i = 0; i < args.length; i++) {
-            System.out.print(args[i]);
+        if (args.length != 0) {
+            for (int i = 0; i < args.length; i++) {
+                System.out.println("Hello, " + args[i]);
+            }
+        } else {
+            System.out.println("Hello, My friend ");
         }
-        System.out.println();
     }
 }
