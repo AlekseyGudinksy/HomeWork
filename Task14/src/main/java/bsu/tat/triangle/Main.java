fe02eb69748b378a94ecfe18f3fe5b2e5ddd084d
Aleksey <alex@MacBook-Pro-Aleksey.local>
2016-11-10 17:57:08 +0300

    Static deleted. Added XML connection in tests

--- Task14/src/main/java/bsu/tat/triangle/Main.java
+++ Task14/src/main/java/bsu/tat/triangle/Main.java
@@ -16,31 +16,33 @@ public class Main {
      */
     public static void main(String[] args) {
 
+        final String HELLO = "Input value of triangle side's";
+        final String SYMBOL_INPUT = "You inputted symbols, not a number";
+        final String LENGTH_OF_A_SIDE = "Length of side a is = ";
+        final String LENGTH_OF_B_SIDE = "Length of side b is = ";
+        final String LENGTH_OF_C_SIDE = "Length of side c is = ";
         double side_a;
         double side_b;
         double side_c;
 
         try {
-
-            System.out.println(Messages.HELLO);
+            System.out.println(HELLO);
             Scanner in = new Scanner(System.in);
             side_a = in.nextDouble();
-            System.out.println(Messages.LENGTH_OF_A_SIDE + side_a);
+            System.out.println(LENGTH_OF_A_SIDE + side_a);
             side_b = in.nextDouble();
-            System.out.println(Messages.LENGTH_OF_B_SIDE + side_b);
+            System.out.println(LENGTH_OF_B_SIDE + side_b);
             side_c = in.nextDouble();
-            System.out.println(Messages.LENGTH_OF_C_SIDE + side_c);
+            System.out.println(LENGTH_OF_C_SIDE + side_c);
 
             Triangle tr = new Triangle(side_a, side_b, side_c);
-            //
-            tr.validateCoefficient();
-
-            tr.outputType();
-            tr.getTriangleType();
+            tr.getValidateCoefficient();
             tr.outputError();
+            tr.getGetTriangleType();
+            tr.outputType();
 
         } catch (Exception e) {
-            System.out.println(Messages.SYMBOL_INPUT);
+            System.out.println(SYMBOL_INPUT);
         }
     }
 }
--- Task14/src/main/java/bsu/tat/triangle/Triangle.java
+++ Task14/src/main/java/bsu/tat/triangle/Triangle.java
@@ -1,11 +1,21 @@
 package bsu.tat.triangle;
 
+
 /**
  * Defines type and validate triangle's sides
  *
  * @author Aleksey Gudinsky
  */
 public class Triangle {
+
+    final String EQUILATERAL = "Triangle is Equilateral";
+    final String ISOSCELES = "Triangle is Isosceles";
+    final String USUAL = "Triangle is Usual";
+    final String TRIANGLE_DONOT_EXIST = "Triangle can't have that length of sides";
+    final String TRIANGLE_EXIST = "Triangle exist.";
+    final String NEGATIVE_LENGTH = "Triangle can't have a side less then 0";
+    final String ZERO_LENGTH = "Triangle can't have a side equal to 0";
+    final String ERROR = "This is not triangle.";
 
     private double a;
     private double b;
@@ -24,15 +34,15 @@ public class Triangle {
      *
      * @return int  which says quantity of same sides
      */
-    int getTriangleType() {
-        if ((a == b) & (b == c) & (c == a)) {
+    private int getTriangleType() {
+        if ((a == b) && (b == c) && (c == a)) {
             return type = 3;
-        }
-        if ((a != b) & (b != c) & (c != a)) {
-            return type = 0;
         }
         if ((a == b) || (b == c) || (c == a)) {
             return type = 2;
+        }
+        if ((a != b) && (b != c) && (c != a)) {
+            return type = 0;
         }
         return type = 666;
     }
@@ -43,7 +53,7 @@ public class Triangle {
      *
      * @return int which indicates type of wrong input
      */
-    int validateCoefficient() {
+    private int validateCoefficient() {
         if ((Double.isInfinite(1 / a)) || (Double.isInfinite(1 / b)) || (Double.isInfinite(1 / c))) {
             return errorNumber = 0;
         }
@@ -60,44 +70,52 @@ public class Triangle {
     /**
      * Printing String message in case of triangle's type
      */
-    void outputType() {
-        if (Double.isInfinite(1 / getType())) {
-            System.out.println(Messages.USUAL);
+    public void outputType() {
+        if (Double.isInfinite(1.0 / getTriangleType())) {
+            System.out.println(USUAL);
         }
         if (getType() == 2) {
-            System.out.println(Messages.ISOSCELES);
+            System.out.println(ISOSCELES);
         }
         if (getType() == 3) {
-            System.out.println(Messages.EQUILATERAL);
+            System.out.println(EQUILATERAL);
         }
         if (getType() == 666) {
-            System.out.println(Messages.ERROR);
+            System.out.println(ERROR);
         }
     }
 
     /**
      * Printing String message in case of triangle's wrong input
      */
-    void outputError() {
+    public void outputError() {
         if (Double.isInfinite(1 / getErrorNumber())) {
-            System.out.println(Messages.ZERO_LENGTH);
+            System.out.println(ZERO_LENGTH);
         }
         if (getErrorNumber() == -1) {
-            System.out.println(Messages.NEGATIVE_LENGTH);
+            System.out.println(NEGATIVE_LENGTH);
         }
         if (getErrorNumber() == 666) {
-            System.out.println(Messages.TRIANGLE_DONOT_EXIST);
+            System.out.println(TRIANGLE_DONOT_EXIST);
         }
         if (getErrorNumber() == 1) {
-            System.out.println(Messages.TRIANGLE_EXIST);
+            System.out.println(TRIANGLE_EXIST);
         }
     }
 
-    public int getErrorNumber() {
-        return errorNumber;
+    public int getGetTriangleType() {
+        return getTriangleType();
+    }
+
+    public int getValidateCoefficient() {
+        return validateCoefficient();
     }
 
     public int getType() {
         return type;
+    }
+
+    public int getErrorNumber() {
+        return errorNumber;
     }
 }
