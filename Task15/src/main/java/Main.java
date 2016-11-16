2c53dbabc15e34f9f629b75a0909a3e6aac7b661
Aleksey <alex@MacBook-Pro-Aleksey.local>
2016-11-17 00:33:51 +0300

    Added 2 classes and pom file on new branch AT-1

---
+++ Task15/src/main/java/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Main is an entry point
+ *
+ * @author Aleksey Gudinsky
+ */
+public class Main {
+    /**
+     * @param args is an argument of command line
+     */
+    public static void main(String[] args) {
+        WD wd = new WD();
+        wd.checkWeb();
+    }
+}
