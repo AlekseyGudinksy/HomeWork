c10beacd70db574644177480f0a7a52e6f96757f
Aleksey <alex@MacBook-Pro-Aleksey.local>
2017-01-05 02:25:58 +0300

    pom file refactored, init test for sum method

---
+++ Task3/src/test/java/bsu/tat/calculator/ComputationTest.java
@@ -0,0 +1,14 @@
+package bsu.tat.calculator;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+
+public class ComputationTest {
+    @Test
+    public void testGetSum() throws Exception {
+
+    }
+
+}
